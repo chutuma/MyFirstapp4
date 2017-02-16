917ba9e1d5e008c142f41c8f0258f579bc7be1b2
chutima <nongjr39@gmail.com>
2017-02-15 23:05:16 -0800

    Initial commit

---
+++ app/src/main/java/com/example/suranits/myfirstapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.suranits.myfirstapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClikmoreinfo (View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.dlt.go.th/th/dlt-knowledge/view.php?_did=111"));
+        startActivity(intent);
+    }
+}
